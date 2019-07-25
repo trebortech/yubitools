93fe08bba1e0fcf019480c41e137efbad7f52956
Robert Booth <robert.booth@trebortech.com>
2019-07-25 18:29:39 -0500

    Create test_connection.py

---
+++ hsm/test_connection.py
@@ -0,0 +1,10 @@
+from yubihsm import YubiHsm
+
+serverip = "127.0.0.1"
+port = 12345
+authid = 1
+password= "password"
+
+hsm = YubiHsm.connect(f"http://{serverip}:{port}/connector/api")
+session = hsm.create_session_derived(authid, password)
+
