9542947edff926b4388faad89c54dcc6941b0696
Robert Booth <robert.booth@trebortech.com>
2019-07-24 22:17:05 -0500

    Update to double brackets

--- hsm/connectionreset.sh
+++ hsm/connectionreset.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 STATUS=$(curl -s localhost:12345/connector/status | grep status=)
-if [ $STATUS != "status=OK" ]; then
+if [[ $STATUS != "status=OK" ]]; then
     for i in /sys/bus/pci/drivers/[uoex]hci_hcd/*:*; do
-      [ -e "$i" ] || continue
+      [[ -e "$i" ]] || continue
       echo "${i##*/}" > "${i%/*}/unbind"
       echo "${i##*/}" > "${i%/*}/bind"
     done
