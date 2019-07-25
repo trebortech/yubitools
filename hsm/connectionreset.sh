19a3c490f55cccd01b4e4a37684a81a76818033f
Robert Booth <robert.booth@trebortech.com>
2019-07-25 13:18:12 -0500

    Add messages to understand what was complete or not

--- hsm/connectionreset.sh
+++ hsm/connectionreset.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 STATUS=$(curl -s localhost:12345/connector/status | grep status=)
+echo $STATUS
 if [[ $STATUS != "status=OK" ]]; then
+    echo "Run reset routine"
     for i in /sys/bus/pci/drivers/[uoex]hci_hcd/*:*; do
       [[ -e "$i" ]] || continue
       echo "${i##*/}" > "${i%/*}/unbind"
       echo "${i##*/}" > "${i%/*}/bind"
     done
+else
+    echo "The reset routine did NOT run"
 fi
